edede927f1d84318ba18bb32ec40fb8ca4728b6e
sanbuphy <physicoada@gmail.com>
2023-02-11 20:22:16 +0800

    fix

--- auto_install_paddle最新版.sh
+++ auto_install_paddle最新版.sh
@@ -94,7 +94,7 @@ case $input in
             wget -c https://developer.download.nvidia.com/compute/cuda/11.2.2/local_installers/cuda_11.2.2_460.32.03_linux.run
             echo " "
             echo "================================="
-            echo "此处打开和安装都需要一些时间...根据机器和网络有所不同可能需要几分钟，请耐心等待"
+            echo "此处打开和安装结束都需要一些时间...根据机器和网络有所不同可能需要几分钟，请耐心等待"
             echo "================================="
             echo " "
             sudo sh cuda_11.2.2_460.32.03_linux.run
@@ -112,7 +112,7 @@ case $input in
             wget -c https://developer.download.nvidia.com/compute/cuda/11.4.4/local_installers/cuda_11.4.4_470.82.01_linux.run
             echo " "
             echo "================================="
-            echo "此处打开和安装都需要一些时间...根据机器和网络有所不同可能需要几分钟，请耐心等待"
+            echo "此处打开和安装结束都需要一些时间...根据机器和网络有所不同可能需要几分钟，请耐心等待"
             echo "================================="
             echo " "
             sudo sh cuda_11.4.4_470.82.01_linux.run
